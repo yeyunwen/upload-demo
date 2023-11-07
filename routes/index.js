d30ff0f6de748e162a723358d56bc526e8546b6f
yeyunwen <1571374338@qq.com>
2023-11-07 23:20:42 +0800

    feat: multer文件上传

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
+const multer = require('multer')
+const fs = require('fs')
+
+const upload = multer({ dest: 'uploads/' })
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+/* POST upload file. */
+router.post('/upload', upload.single('file'), function(req, res, next) {
+  const fileName = req.file.originalname
+  const fileContent = fs.readFileSync(req.file.path)
+  fs.writeFileSync(`uploads/${fileName}`, fileContent)
+  
+  res.send(req.file);
 });
 
 module.exports = router;
