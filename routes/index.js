4229ee83e198875cd84e23171c644ebc8c9d4e52
yeyunwen <1571374338@qq.com>
2023-11-12 19:02:24 +0800

    style: format

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,22 @@
-var express = require('express');
-const multer = require('multer')
-const fs = require('fs')
+var express = require("express");
+const multer = require("multer");
+const fs = require("fs");
 
-const upload = multer({ dest: 'uploads/' })
+const upload = multer({ dest: "uploads/" });
 
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-
 /* POST upload file. */
-router.post('/upload', upload.single('file'), function(req, res, next) {
-  const fileName = req.file.originalname
-  const fileContent = fs.readFileSync(req.file.path)
-  fs.writeFileSync(`uploads/${fileName}`, fileContent)
-  
+router.post("/upload", upload.single("file"), function (req, res, next) {
+  const fileName = req.file.originalname;
+  const fileContent = fs.readFileSync(req.file.path);
+  fs.writeFileSync(`uploads/${fileName}`, fileContent);
+
   res.send(req.file);
 });
 
